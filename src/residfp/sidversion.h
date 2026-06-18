92a09bdc45e920ab36452b767b0cc4d1ba9a2c80
yousernaym <mickeh.se@hotmail.com>
2026-06-18 22:35:09 +0200

    Visual Studio project

---
+++ src/residfp/sidversion.h
@@ -0,0 +1,14 @@
+#ifndef LIBRESIDFP_VERSION_H
+#define LIBRESIDFP_VERSION_H
+
+#ifndef RESIDFP_H
+#  error Do not include directly.
+#endif
+
+/* Generated from sidversion.h.in for the Visual Music MSVC build (libresidfp 1.0.2). */
+
+#define LIBRESIDFP_VERSION_MAJ 1
+#define LIBRESIDFP_VERSION_MIN 0
+#define LIBRESIDFP_VERSION_LEV 2
+
+#endif
